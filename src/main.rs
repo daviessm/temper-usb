416c31f5b76bd13dac43e6dbcbdc3969e840ba1a
Steven Davies <github@steev.me.uk>
2021-09-30 09:23:38 +0100

    Run forever rather than forking

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,17 @@ mod mqtt;
 mod temper;
 
 use args::Args;
-use std::{
-    convert::TryInto,
-    sync::{Arc, Mutex},
-};
+use std::{convert::TryInto, sync::{Arc, Mutex}, thread, time::Duration};
 
-use actix_web::{App, HttpServer, web::Data};
-use fork::{fork, Fork};
+use actix_web::{App, HttpServer, rt::System, web::Data};
+use futures::executor::block_on;
 use sd_notify::NotifyState;
 
 use crate::mqtt::MqttConnection;
 
 #[actix_web::main]
 async fn main() {
-    let args = Args::parse().unwrap();
+    let args = Arc::new(Args::parse().unwrap());
     let temper = temper::iterate_usb_devices().unwrap();
     let shared_temper = Arc::new(Mutex::new(temper));
     let shared_temper_clone1 = shared_temper.clone();
@@ -25,37 +22,38 @@ async fn main() {
     let mut ready = false;
 
     if args.mqtt() {
-        match fork() {
-            Ok(Fork::Child) => {
-                let mqtt_cli = MqttConnection::new(
-                    shared_temper,
-                    args.mqtt_server(),
-                    args.mqtt_topic().to_string(),
-                    args.mqtt_frequency().try_into().unwrap(),
-                );
+        let args = args.clone();
+        thread::spawn(move || {
+            let mqtt_cli = MqttConnection::new(
+                shared_temper,
+                args.mqtt_server(),
+                args.mqtt_topic().to_string(),
+                args.mqtt_frequency().try_into().unwrap(),
+            );
 
-                mqtt_cli.run_forever();
-            }
-            Ok(Fork::Parent(_)) => ready = true,
-            Err(_) => panic!("Fork failed"),
-        }
+            mqtt_cli.run_forever();
+        });
+        ready = true;
     }
     if args.http() {
-        match fork() {
-            Ok(Fork::Child) => {
-                let http_server = HttpServer::new(move || {
+        let args = args.clone();
+        thread::spawn(move || {
+            let sys = System::new("http-server");
+            block_on(
+                HttpServer::new(move || {
                     App::new()
                         .service(http::index)
                         .app_data(Data::new(shared_temper_clone1.clone()).clone())
                 })
                 .bind(format!("0.0.0.0:{}", args.http_port()))
                 .unwrap()
-                .run();
-                http_server.await.unwrap();
-            }
-            Ok(Fork::Parent(_)) => ready = true,
-            Err(_) => panic!("Fork failed"),
-        }
+                .run(),
+            )
+            .unwrap();
+
+            sys.run().unwrap();
+        });
+        ready = true;
     }
 
     if ready {
@@ -65,5 +63,9 @@ async fn main() {
     //If no requests for MQTT or HTTP, output a value and end
     if !args.http() && !args.mqtt() {
         println!("{}", shared_temper_clone2.lock().unwrap().get_temp());
+    } else {
+        loop {
+            thread::sleep(Duration::new(1, 0));
+        }
     }
 }
